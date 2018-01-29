9ef6dba31986974f29d4921d5c35b516de0549da
qt <qt@QTITPC049>
2018-01-29 10:49:18 +0800

    测试

--- src/main/java/com/maxtt/crawler/demomain/demo1.java
+++ src/main/java/com/maxtt/crawler/demomain/demo1.java
@@ -6,9 +6,11 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+//test
 public class demo1 {
 	public static void main(String[] args) {
 		doGetDemo("http://www.baidu.com");
+		System.err.println("////////////");
 	}
 	
 	/***
